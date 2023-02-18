8ac8383d9c17710a26d14bb23a6b0fca5d2a1e50
kyon <kenshiro.araki@gmail.com>
2023-02-19 00:21:01 +0900

    ソリューションがビルド通らなかったので修正

--- LearningSpace/App/Properties/Resources.Designer.cs
+++ LearningSpace/App/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LearningSpace.App.Properties {
+namespace LearningSpace.Main.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LearningSpace.App.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LearningSpace.App.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LearningSpace.Main.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
